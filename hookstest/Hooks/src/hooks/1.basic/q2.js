2a981dc551d3a2d8bfa7eac551e80ab302565a97
taeginote <ab315601@naver.com>
2023-02-18 00:12:22 +0900

    Add : 1.basic - q2 베스트 선정 후 커밋했습니다.

--- hookstest/Hooks/src/hooks/1.basic/q2.js
+++ hookstest/Hooks/src/hooks/1.basic/q2.js
@@ -1,45 +1,39 @@
 import { useRef, useState } from "react";
 
 function Q2() {
-    // 2-1
-    const arr = useRef([]);
-    const inputValue = useRef();
-    const [state, setState] = useState(false);
     const [forceRender, setForceRender] = useState(false);
+    const [view, setView] = useState(false);
+    const inputChange = useRef();
+    const arr = useRef([]);
+    //2-1번
 
     const onAddList = () => {
         setForceRender((prev) => !prev);
-        arr.current.push({ value: inputValue.current.value });
-        // setState(false);
+        arr.current.push({ value: inputChange.current.value });
     };
 
-    const onClickListAdd = () => {
-        setState(true);
+    const onMakeList = () => {
+        setView(true);
     };
 
-    // 2-2
-    const colorChangeBtn = useRef();
+    //2-2번
+    const colorChange = useRef();
 
-    const colorBtn = () => {
-        colorChangeBtn.current.style = "color: red";
+    const onBtnColor = () => {
+        colorChange.current.style = "color : red";
     };
     /* 
     문제2
 
     2-1)
-        1. 추가를 누르면 배열에 값이 추가된다
-          1. 누르면 인풋 실시간 값을 가져와야함
-          2. 인풋 값을 arr에 넣어야한다.
-        2. 제출을 누르면 배열값들을 보여줘야한다.
-          1.제출을 누르면 arr 데이터를 다 보여줄수 있어야한다.
-
         useRef에 관련한 문제입니다.
 
         추가 버튼을 누르면 input에 있던 value는 배열 arr에 추가됩니다.
         그러나, 추가 버튼을 누를 때마다 강제 랜더링 상태가 무조건 적으로 업데이트 됩니다.
-
+        
+~
         이러한 상황에서 제출버튼을 누르면
-
+        
         지금까지 추가하였던 목록 배열(arr)이 
         <ul>의 li의 항목으로 추가되어야합니다.
 
@@ -55,7 +49,7 @@ function Q2() {
         문제 2-2는 변경 버튼을 클릭하면
         p태그의 색상이 다른 색상으로 변경됩니다.
         
-        이는 state를 사용하는 것이 가장 올바른 방법이지만
+        이는 satet를 사용하는 것이 가장 올바른 방법이지만
         어를 사용할 수 없는 어쩔 수 없는 상황에 놓여있습니다.
 
         따라서 useRef는 사용하여 해당 문구의 색상을 변경해보세요 :)
@@ -67,24 +61,29 @@ function Q2() {
             <div>
                 <h2>문제 2-1</h2>
                 <p>
-                    <input ref={inputValue} />
+                    <input ref={inputChange} />
                 </p>
+
                 <p>
                     <button onClick={onAddList}>추가</button>
                 </p>
                 <p>
-                    <button onClick={onClickListAdd}>제출</button>
+                    <button onClick={onMakeList}>제출</button>
                 </p>
-                {!state ? (
-                    <p>제출된 목록이 없습니다</p>
-                ) : (
-                    <ul>{state && arr.current.map((el) => <li>{el.value}</li>)}</ul>
-                )}
+                <ul>
+                    {view ? (
+                        arr.current.map((el) => <li>{el.value}</li>)
+                    ) : (
+                        <p>제출된 목록이 없습니다</p>
+                    )}
+                </ul>
             </div>
             <div>
                 <h2>문제 2-2</h2>
-                <p ref={colorChangeBtn}> 이 문구는 아래 버튼을 누르면 색상이 바뀝니다</p>
-                <button onClick={colorBtn}>변경</button>
+                <p ref={colorChange}>
+                    이 문구는 아래 버튼을 누르면 색상이 바뀝니다
+                </p>
+                <button onClick={onBtnColor}>변경</button>
             </div>
         </>
     );
