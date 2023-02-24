e5d1bcf2b218339f16a3442d96531e8f997ac91b
minsik0726 <kimminsik0726@gmail.com>
2023-02-24 22:59:46 +0900

    Feat: q3, state1 best 선정

--- hookstest/Hooks/src/hooks/2.state/state1.js
+++ hookstest/Hooks/src/hooks/2.state/state1.js
@@ -1,7 +1,8 @@
-import PlayListMock from "../../mock/playList.json";
+import { useState } from "react";
+import PlayListMock from "../../__mock__/playList.json";
 
 function State1() {
-    /* 
+  /* 
     문제 1.
 
     state를 다루기 위한 기초 문제입니다.
@@ -11,32 +12,71 @@ function State1() {
     삭제 버턴을 눌렀을 때 데이터가 삭제될 수 있도록 해주세요
   */
 
-    console.log(PlayListMock.playlist);
-    /*데이터 콘솔에 찍어두었으니 확인해볼 것 */
+  console.log(PlayListMock.playlist);
+  /* 데이터 콘솔에 찍어두었으니 확인해볼 것 */
 
-    return (
-        <>
-            <h1>문제1</h1>
-            <ul>
-                {/ list /}
-                {/ 예시 데이터입니다 */}
-                <li>
-                    <h3>Summer</h3>
-                    <p>Joe Hisaishi</p>
-                </li>
-            </ul>
-            <div>
-                <p>
-                    곡명 : <input />
-                </p>
-                <p>
-                    가수/작곡 : <input />
-                </p>
-                <p>
-                    <button>추가</button>
-                </p>
-            </div>
-        </>
-    );
+  const [lists, setLists] = useState(PlayListMock.playlist);
+
+  const [inputs, setInputs] = useState({
+    title: "",
+    signer: "",
+  });
+
+  const { title, signer } = inputs;
+
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onAddList = () => {
+    const newList = {
+      title,
+      signer,
+    };
+
+    setLists([...lists, newList]);
+
+    setInputs({
+      title: "",
+      signer: "",
+    });
+  };
+
+  const removeBtn = (title) => {
+    setLists(lists.filter((v) => v.title !== title));
+  };
+
+  return (
+    <>
+      <h1>문제1</h1>
+      <ul>
+        {lists.map((v) => (
+          <li>
+            <h3>{v.title}</h3>
+            <p>{v.signer}</p>
+            <button onClick={() => removeBtn(v.title)}>삭제</button>
+            <hr></hr>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <p>
+          곡명 : <input name="title" value={title} onChange={onInputChange} />
+        </p>
+        <p>
+          가수/작곡 :{" "}
+          <input name="signer" value={signer} onChange={onInputChange} />
+        </p>
+        <p>
+          <button onClick={onAddList}>추가</button>
+        </p>
+      </div>
+    </>
+  );
 }
 export default State1;
